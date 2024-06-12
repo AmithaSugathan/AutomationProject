f6f25474aa30fa57265b3691d9213d90cc127df4
Amitha <Amitha@LAPTOP-NPQ5HL7M>
2024-06-12 15:50:48 +0530

    Base.java class for automationtraining

---
+++ src/test/java/testscript/Base.java
@@ -0,0 +1,16 @@
+package testscript;
+
+public class Base {
+	
+	public void InitializeBrowser()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		
+Base base=new Base();
+base.InitializeBrowser();
+	}
+
+}
